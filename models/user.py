479a2bcf1ac6ae7e1298afa25dc47e49fe396456
NathanielBirhanu <nbirhanu98@gmail.com>
2023-12-12 02:39:12 -0800

    updated

--- models/user.py
+++ models/user.py
@@ -1,9 +1,19 @@
 #!/usr/bin/python3
+"""Defines the User class."""
 from models.base_model import BaseModel
-class user(BaseModel):
-    def __init__(self):
-        super().__init__()
-        self.email = ""
-        self.password = ""
-        self.first_name = ""
-        self.last_name = ""
+
+
+class User(BaseModel):
+    """Represent a User.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""

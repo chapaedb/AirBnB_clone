d7c98e6754479b1f5d0019d33970846ddf326d33
NathanielBirhanu <nbirhanu98@gmail.com>
2023-12-12 03:16:25 -0800

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,23 +10,40 @@ from models.amenity import Amenity
 from models.review import Review
 import os.path
 
+
 class FileStorage:
+    """
+    FileStorage class handles saving and loading objects to and from a JSON file.
+    """
+
     __file_path = os.path.join(os.getcwd(), "file.json")
     __objects = {}
 
     def all(self):
+        """
+        Returns the dictionary of all objects.
+        """
         return self.__objects
 
     def new(self, obj):
-        key = f"{obj.__class__.__name__}.id"
+        """
+        Adds a new object to the storage dictionary.
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def default_serializer(self, obj):
+        """
+        Serializes the object into a dictionary.
+        """
         if isinstance(obj, BaseModel):
             return obj.to_dict()
-        raise TypeError("Object of type {} is not JSON serializable".format(type(obj)))
+        raise TypeError(f"Object of type {type(obj)} is not JSON serializable")
 
     def save(self):
+        """
+        Serializes all objects and saves them to the JSON file.
+        """
         data = {}
         for key, obj in self.__objects.items():
             data[key] = obj.to_dict()
@@ -34,6 +51,9 @@ class FileStorage:
             json.dump(data, file, default=self.default_serializer)
 
     def reload(self):
+        """
+        Loads the JSON file and reconstructs the objects.
+        """
         if os.path.exists(self.__file_path):
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
